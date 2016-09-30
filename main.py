1392a362cf34bf4531852ce0648f6918e35fd11b
crutchcorn <crutchcorn@gmail.com>
2016-09-30 08:04:38 -0700

    Moved uniprez from inline code to library.
    See https://github.com/crutchcorn/Uniprez for more

--- main.py
+++ main.py
@@ -11,36 +11,15 @@ Options:
 
 """
 from docopt import docopt
-from pkg_resources import iter_entry_points
 from os import rename
 from wget import download
 from re import sub
+from uniprez import getExtensions
 
 arguments = docopt(__doc__, version='DriverDownloader 0.0.1')
 
-##### THE FOLLOWING IS CODE IN AN UPCOMING PROJECT. THIS WILL EVENTUALLY BE MOVED INTO A LIBRARY!
-extensions = {}
-handleExtPlugin = {}
 group = "driverdl.plugin"
-for entry_point in iter_entry_points(group=group, name=None):
-    if entry_point.attrs[0] == "handleExt":
-        if extensions:
-            for extension in extensions:
-                if extensions[extension] == entry_point.module_name: # if extensions["mp3"] == "plugins.lenovo "(this assumes this is second)
-                    extensions[extension] = entry_point.load()
-        handleExtPlugin[entry_point.module_name] = entry_point.load() # handleExtPlugin["plugins.lenovo"] (this assumes this is first)
-    elif entry_point.attrs[0] == "getExt":
-        if handleExtPlugin.get(entry_point.module_name, None): # handleExtPlugin["plugins.lenovo"] (this assumes this is second)
-            moduleLoad = handleExtPlugin[entry_point.module_name]
-        else:
-            moduleLoad = entry_point.module_name # This assumed this is first. It allows the extensions to become module_name to be handled by handleExt code
-        for extension in entry_point.load()(): # entry_point() would return list of extentions handleExt could handle
-            extensions[extension] = moduleLoad # extensions["mp3"]
-    elif not entry_point.attrs[0]:
-        print("But nobody came")
-###### WHEN THAT IS DONE, PLEASE REMOVE THIS CODE AND REPLACE IT WITH THE LIBRARY
-
-
+extensions = getExtensions(group)
 urlExt = sub(r'http.*//', '', arguments["<url>"]) # Removes http/https
 urlExt = urlExt.split('/')[0].split('.')[-2] # Gets second to last string before /, split by . (support.lenovo.com => lenovo)
 drivers = extensions[urlExt](arguments["<url>"]) # it lenovo.com will run lenovo, but www.lenovo.com will run www This should be fixed
@@ -59,5 +38,3 @@ for driver in drivers:
         rename(file_name, driver['Name']+"ALT") # Should continue to add "ALT" until no longer possible
     except NameError:
         pass
-
-# http://docs.pylonsproject.org/projects/pylons-webframework/en/latest/advanced_pylons/entry_points_and_plugins.html
